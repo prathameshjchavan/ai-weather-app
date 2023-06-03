f46445b426cdc95dbd8c4ef0d0ebbfce9e3526b3
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-06-03 17:16:37 +0530

    building homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,22 @@
 "use client";
 
-import { Card, Text, Subtitle } from "@tremor/react";
+import { Card, Text, Subtitle, Divider } from "@tremor/react";
 
 export default function Home() {
 	return (
-		<main>
-			<Card>
-				<Text>Weather AI</Text>
-				<Subtitle>
+		<div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center items-center">
+			<Card className="max-w-4xl mx-auto">
+				<Text className="text-6xl font-bold text-center mb-10">Weather AI</Text>
+				<Subtitle className="text-xl text-center">
 					Powered by OpenAI, Next.js 13.3, Tailwind CSS, Tremor 2.0 + More!
 				</Subtitle>
+
+				<Divider className="my-10" />
+
+				<Card className="bg-gradient-to-br from-[#394F68] to-[#183B7E]">
+					{/* City Picker */}
+				</Card>
 			</Card>
-		</main>
+		</div>
 	);
 }
