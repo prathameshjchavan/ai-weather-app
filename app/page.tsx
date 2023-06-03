47e276740ee08f5bb8c3202b840e4364575cfd1f
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-06-03 12:48:55 +0530

    setting up tremor

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+
+import { Card, Text, Subtitle } from "@tremor/react";
+
 export default function Home() {
 	return (
 		<main>
-			<h1>Let's build a GPT-4 Weather App</h1>
+			<Card>
+				<Text>Weather AI</Text>
+				<Subtitle>
+					Powered by OpenAI, Next.js 13.3, Tailwind CSS, Tremor 2.0 + More!
+				</Subtitle>
+			</Card>
 		</main>
 	);
 }
