9990f08906f2c631177d87d7154fa1155e2745e5
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-06-04 18:13:08 +0530

    building stat cards

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,5 +1,6 @@
 import { getClient } from "@/apollo-client";
 import CalloutCard from "@/components/CalloutCard";
+import StatCard from "@/components/StatCard";
 import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQuery";
 
 type Props = {
@@ -23,7 +24,6 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
 		},
 	});
 	const results: Root = data.myQuery;
-	console.log(results);
 
 	return (
 		<div>
@@ -40,9 +40,53 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
 						</p>
 					</div>
 
-					<div>
+					<div className="m-2 mb-10">
 						<CalloutCard message="This is where GPT-4 Summary will go" />
 					</div>
+
+					<div className="grid grid-cols-1 xl:grid-cols-2 gap-5 m-2">
+						<StatCard
+							title="Maximum Temparture"
+							metric={`${results.daily.temperature_2m_max[0].toFixed(1)}°`}
+							color="yellow"
+						/>
+						<StatCard
+							title="Minimum Temparture"
+							metric={`${results.daily.temperature_2m_min[0].toFixed(1)}°`}
+							color="green"
+						/>
+						<div>
+							<StatCard
+								title="UV Index"
+								metric={`${results.daily.uv_index_max[0].toFixed(1)}°`}
+								color="rose"
+							/>
+							{Number(results.daily.uv_index_max[0].toFixed(1)) > 5 && (
+								<CalloutCard
+									message="The UV is high today, be sure to wear SPF!"
+									warning
+								/>
+							)}
+						</div>
+						<div className="flex space-x-3">
+							<StatCard
+								title="Wind Speed"
+								metric={`${results.current_weather.windspeed.toFixed(1)}m/s`}
+								color="cyan"
+							/>
+							<StatCard
+								title="Wind Direction"
+								metric={`${results.current_weather.winddirection.toFixed(1)}°`}
+								color="violet"
+							/>
+						</div>
+					</div>
+				</div>
+				<hr className="mb-5" />`
+				<div className="space-y-3">
+					{/* TempChart */}
+					{/* RainChart */}
+					{/* HumidityChart */}
 				</div>
 			</div>
 		</div>
